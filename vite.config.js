80a63fbb9cf41946a02c5dfa94e00a0ebc53f593
alfin0616 <alfinsujuda06@gmail.com>
2026-05-03 22:36:08 +0700

    Fix config error di Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
+// Konfigurasi standar untuk Vite 5
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
 })
